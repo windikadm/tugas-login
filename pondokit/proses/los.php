e755b261d7b170bb243be6b72a0de271536dfe10
windikadm <31056981+windikadm@users.noreply.github.com>
2017-12-13 16:11:34 +0700

    tugas login

---
+++ pondokit/proses/los.php
@@ -0,0 +1,22 @@
+<?php
+$connect = mysqli_connect("localhost", "root", "", "pondok_it");
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+  if (!empty($email) && !empty ($password)) {
+    $sql = mysqli_query($connect,"SELECT * FROM user_id WHERE email='$email' and password='$password'");
+    $result = mysqli_num_rows($sql);
+    if ($result) {
+      $row = mysqli_fetch_array($sql);
+      session_start();
+      $_SESSION['login']= true;
+      $_SESSION['email']= $email;
+      $_SESSION['nama']= $row['nama'];
+      echo "login berhasil";
+    } else {
+      echo "Email dan password anda salah";
+    }
+  }
+  else {
+    echo "Email dan password anda kosong, silahkan diisi.";
+  }
+?>
